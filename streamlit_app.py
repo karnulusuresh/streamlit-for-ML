67381690fa84ac91ad9f0fa42d34b1c123d2085a
Karnulu Suresh <karnulusuresh729@gmail.com>
2026-04-29 18:40:46 +0530

    Implement DataFrame creation and concatenation
    Added functionality to create a DataFrame from user inputs and concatenate with existing data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,14 @@ with st.sidebar:
   bill_depth_mm = st.slider('bill depth (mm)', 13.1, 21.5, 17.2)
   flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('body mass', 2700.0, 6300.0, 4207.0)
+
+data = st.DataFrame('Island': Island,
+                    'bill_length_mm': bill_length_mm,
+                    'bill_depth_mm': bill_depth_mm,
+                    'flipper_length_mm': flipper_length_mm,
+                    'body_mass_g': body_mass_g,
+                    'gender': gender)
+input_df = pd.DataFrame(data, index[0])
+input_penguins = pd.concat([input_df, x],axis =0)
+
+input_penguins
