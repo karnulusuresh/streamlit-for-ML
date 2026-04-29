62dc822fcfacf5ad2ed6b54e1a2ac21f6eb31ba9
Karnulu Suresh <karnulusuresh729@gmail.com>
2026-04-29 17:04:08 +0530

    Import pandas and load penguin dataset
    Added pandas import and loaded dataset from URL.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title(' Machine Learning App')
 
 st.info('This is app build machine learning models')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguin_cleaned.csv')
+df
