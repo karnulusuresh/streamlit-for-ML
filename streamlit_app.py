4aed67b12ac1d946d8a9fdfd1f00733ec2d7b521
Karnulu Suresh <karnulusuresh729@gmail.com>
2026-04-29 17:14:43 +0530

    Add data expander and display raw data in app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,13 @@ import pandas as pd
 st.title(' Machine Learning App')
 
 st.info('This is app build machine learning models')
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+  st.write('**X**')
+  x = df.drop('species', axis = 1)
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+  st.write('**Y**')
+  y = df.species
+
