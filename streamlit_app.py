7d9fee16f117d877ccc8c4885c097a456600154c
Karnulu Suresh <karnulusuresh729@gmail.com>
2026-04-29 18:42:52 +0530

    Fix DataFrame syntax and input initialization
    Fixed syntax errors in DataFrame creation and corrected input DataFrame initialization.

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,12 +28,12 @@ with st.sidebar:
   flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('body mass', 2700.0, 6300.0, 4207.0)
 
-data = st.DataFrame('island': island,
+data = st.DataFrame{'island': island,
                     'bill_length_mm': bill_length_mm,
                     'bill_depth_mm': bill_depth_mm,
                     'flipper_length_mm': flipper_length_mm,
                     'body_mass_g': body_mass_g,
-                    'gender': gender)
+                    'gender': gender}
 input_df = pd.DataFrame(data, index[0])
 input_penguins = pd.concat([input_df, x],axis =0)
 
