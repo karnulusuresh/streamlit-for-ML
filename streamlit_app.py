ad07d6e89311dfee7852b370aef2f6ad362464fd
Karnulu Suresh <karnulusuresh729@gmail.com>
2026-04-29 18:24:41 +0530

    Fix typo in flipper length slider label

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,5 @@ with st.sidebar:
   gender = st.selectbox('Gender', {'male', 'female'})
   bill_length_mm = st.slider('bill length (mm)', 32.1, 59.6, 43.9)
   bill_depth_mm = st.slider('bill depth (mm)', 13.1, 21.5, 17.2)
-  flipper_length_mm = st.slider('flipper length (mm), 172.0, 231.0, 201.0)
+  flipper_length_mm = st.slider('flipper length (mm)', 172.0, 231.0, 201.0)
   body_mass_g = st.slider('body mass', 2700.0, 6300.0, 4207.0)
